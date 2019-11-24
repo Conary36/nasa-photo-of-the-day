200aae3a6e5c978f55ac2ebbc88493d7045e85b9
Conary36 <cbeckford144@gmail.com>
2019-11-23 21:19:25 -0500

    added transition effect

--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled, {keyframes} from 'styled-components';
 
 
+
 const rotate360 = keyframes`
   from {
     transform: rotate(0deg);
@@ -10,6 +11,15 @@ const rotate360 = keyframes`
     transform: rotate(360deg);
   }
 `
+// const expand = keyframes`
+//   from {
+//       transform: width 1s ease-in-out, padding-left 1s ease-in-out, 
+//         padding-right 1s ease-in-out;;
+//   }
+//   to {
+//       transform: width(90%);
+//   }
+// `
 
 const Info = styled.p `
     border: 3px groove blue;
@@ -26,14 +36,23 @@ const Title = styled.h1`
 const PicDiv = styled.div`
     width: 70%;
     height: 50%;
-
+    margin: 2%;
+    
 `
 const Img = styled.img`
     width: 100%;
+    transition: width 2s linear 
+    &:hover{
+        width: 50%;
+        
+    }  
 `
+
 const Container = styled.div`
+  width: 100%;
   background: linear-gradient(to left, #fff, #076585, #acb6e5, #86fde8);
-`;
+`
+
 const Section = styled.section`
     display: flex;
     
@@ -45,14 +64,12 @@ const InfoDiv = styled.div`
 const NasaCard = ({date, content, hdurl}) =>{
 
 
-
-
     return(
         <Container>
             <Title>Vision of the Void</Title>
             <p>{date}</p>
             <Section>
-                <PicDiv><Img src={hdurl} alt='space pic'/></PicDiv>
+                <PicDiv><Img src={hdurl} alt='space pic' /></PicDiv>
                 <InfoDiv>
                     <Info>{content}</Info>
                 </InfoDiv>
